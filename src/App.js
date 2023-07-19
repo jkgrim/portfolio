fb4fd420b63ffcd6a3c746ea923ed4bc726a9ef9
jkgrim <j.grimshaw37@gmail.com>
2023-07-19 17:33:53 -0600

    adds and styles nav, adds images and introduction

--- src/App.js
+++ src/App.js
@@ -1,4 +1,101 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faEnvelope,
+  faCircleChevronRight,
+} from "@fortawesome/free-solid-svg-icons";
+import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
+import "./styles/app.scss";
+import companyLogo from "./images/main-logo.png";
+import me from "./images/me.png";
+import swirlies from "./images/swirlies.png";
+import circles from "./images/circles.png";
+
 function App() {
-  return <div className="App">Hello World</div>;
+  return (
+    <div className="portfolio-container">
+      <div className="nav-container">
+        <div className="home-wrapper">
+          <img src={companyLogo} alt="Jacob Grimshaw Company Logo" />
+          JG
+        </div>
+
+        <div className="media-wrapper">
+          <a href="https://github.com/jkgrim" target="_blank" rel="noreferrer">
+            <FontAwesomeIcon icon={faGithub} />
+          </a>
+
+          <a
+            className="linked-in"
+            href="https://www.linkedin.com/in/jkgrim/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <FontAwesomeIcon icon={faLinkedin} />
+          </a>
+
+          <a href="mailto:jacobkgrimshaw@gmail.com">
+            <FontAwesomeIcon icon={faEnvelope} />
+          </a>
+        </div>
+      </div>
+
+      <img
+        className="swirlies"
+        src={swirlies}
+        alt="swirlies to fill in white space"
+      />
+
+      <img
+        className="circles"
+        src={circles}
+        alt="circles to fill in white space"
+      />
+
+      <div className="introduction-container">
+        <div className="introduction-wrapper">
+          <div className="intro-header top">Nice to meet you!</div>
+          <div className="intro-header">
+            I'm <em>Jacob Grimshaw</em>.
+          </div>
+
+          <div className="subtext-wrapper">
+            <div className="subtext">
+              I am a Front End developer with industry experience building
+              websites and web applications.
+            </div>
+
+            <div className="subtext">
+              I specialize in React and JavaScript and have professional
+              experience working with SQLAlchemy and Python.
+            </div>
+
+            <div className="subtext bottom">
+              I love to build, problem solve, as well as create, and I am
+              passionate about what I do!
+            </div>
+          </div>
+
+          <div className="contact-me">
+            <a href="mailto:jacobkgrimshaw@gmail.com">CONTACT ME</a>
+          </div>
+
+          <div className="view-portfolio-wrapper">
+            <div className="view-portfolio">
+              <a href="https://github.com/jkgrim">
+                View Portfolio
+                <FontAwesomeIcon icon={faCircleChevronRight} />
+              </a>
+            </div>
+          </div>
+        </div>
+
+        <div className="self-img">
+          <img src={me} alt="a very handsome man" />
+        </div>
+      </div>
+
+      <hr />
+    </div>
+  );
 }
 export default App;
